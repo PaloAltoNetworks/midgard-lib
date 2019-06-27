97a7176db8a9b34815e4aa11d40649cad9d155ce
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-06-27 13:32:11 -0700

    new: alias claims.MidgardClaims gaia types.MidgardClaims

--- claims/claims.go
+++ claims/claims.go
@@ -11,22 +11,13 @@
 
 package claims
 
-import jwt "github.com/dgrijalva/jwt-go"
+import (
+	"go.aporeto.io/gaia/types"
+)
 
-// MidgardClaims is a struct to represeting the data some a Midgard issued claims.
-type MidgardClaims struct {
-	Realm  string            `msgpack:"realm" json:"realm"`
-	Quota  int               `msgpack:"quota,omitempty" json:"quota,omitempty"`
-	Data   map[string]string `msgpack:"data" json:"data"`
-	Opaque map[string]string `msgpack:"opaque,omitempty" json:"opaque,omitempty"`
-
-	jwt.StandardClaims
-}
+type MidgardClaims = types.MidgardClaims
 
 // NewMidgardClaims returns a new Claims.
 func NewMidgardClaims() *MidgardClaims {
-	return &MidgardClaims{
-		Data:           map[string]string{},
-		StandardClaims: jwt.StandardClaims{},
-	}
+	return types.NewMidgardClaims()
 }
