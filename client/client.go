3b2126c0314ba4b02b87fd06d071af18c28d1fde
Philip Hamer <philip@aporeto.com>
2019-12-12 12:34:31 -0500

    add import for tglib

--- client/client.go
+++ client/client.go
@@ -30,6 +30,7 @@ import (
 	"go.aporeto.io/gaia"
 	"go.aporeto.io/midgard-lib/ldaputils"
 	"go.aporeto.io/midgard-lib/tokenmanager/providers"
+	"go.aporeto.io/tg/tglib"
 )
 
 // A Client allows to interract with a midgard server.
