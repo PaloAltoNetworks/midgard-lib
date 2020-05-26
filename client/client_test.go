477acf21fcc0b9ce54c4f6bc2dda575c57a9ae21
Antoine <primalmotion@users.noreply.github.com>
2020-05-26 12:30:26 -0700

    new: implement IssueFromAporetoIdentityToken (#38)

--- client/client_test.go
+++ client/client_test.go
@@ -396,6 +396,53 @@ func TestClient_IssueFromVince(t *testing.T) {
 	})
 }
 
+func TestClient_IssueFromAporetoIdentityToken(t *testing.T) {
+
+	Convey("Given I have a client and a fake working server", t, func() {
+
+		expectedRequest := gaia.NewIssue()
+
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if err := json.NewDecoder(r.Body).Decode(expectedRequest); err != nil {
+				panic(err)
+			}
+			fmt.Fprintln(w, `{"data": "","realm": "aporetoidentitytoken","token": "yeay!"}`)
+		}))
+		defer ts.Close()
+
+		cl := NewClient(ts.URL)
+
+		Convey("When I call IssueFromAporetoIdentityToken", func() {
+
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+			defer cancel()
+
+			token, err := cl.IssueFromAporetoIdentityToken(ctx, "in-token", 1*time.Minute,
+				OptQuota(1),
+				OptRestrictNamespace("/ns1"),
+				OptRestrictPermissions([]string{"@auth:role=toto"}),
+				OptRestrictNetworks([]string{"127.0.0.0/8"}),
+			)
+
+			Convey("Then err should be nil", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("Then the issue request should be correct", func() {
+				So(expectedRequest.Realm, ShouldEqual, "AporetoIdentityToken")
+				So(expectedRequest.Metadata["token"], ShouldEqual, "in-token")
+				So(expectedRequest.RestrictedPermissions, ShouldResemble, []string{"@auth:role=toto"})
+				So(expectedRequest.RestrictedNamespace, ShouldEqual, "/ns1")
+				So(expectedRequest.RestrictedNetworks, ShouldResemble, []string{"127.0.0.0/8"})
+			})
+
+			Convey("Then token should be correct", func() {
+				So(token, ShouldEqual, "yeay!")
+			})
+		})
+	})
+}
+
 func TestClient_IssueFromTwistlock(t *testing.T) {
 
 	Convey("Given I have a client and a fake working server", t, func() {
