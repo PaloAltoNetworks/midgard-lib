4e179ca9e0e6e0e8091f760a11d9c75d39d1fc81
ericrpowers <epowers@paloaltonetworks.com>
2021-06-18 12:33:57 -0700

    Update: Revert enforcing OptAudience deprecation

--- client/options.go
+++ client/options.go
@@ -47,8 +47,7 @@ func OptOpaque(opaque map[string]string) Option {
 }
 
 // OptAudience passes the requested audience for the token.
-//
-// Deprecated: Using OptAudience is deprecated. Switch to OptLimitAuthz.
+// Using OptAudience is deprecated. Switch to OptLimitAuthz. (TODO: Find real mapping as OptLimitAuthz does not exist)
 func OptAudience(audience string) Option {
 
 	return func(opts *issueOpts) {
